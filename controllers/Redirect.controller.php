3362b99eec2216cc81f836a0d284f8c82c2f7a14
salache <lfsalache@gmail.com>
2023-05-17 21:44:14 -0300

    Update Rotas

--- controllers/Redirect.controller.php
+++
@@ -1,7 +0,0 @@
-<?php
-    Class Redirect
-    {
-        public function telaLogin(){
-            require "localhost/projeto_festas.com/views/login.view.php";
-        }
-    }
--- index.php
+++ index.php
@@ -2,16 +2,30 @@
 session_start();
 require 'controllers/Event.controller.php';
 require 'vendor/autoload.php';
-use CoffeeCode\Router\Router;
-$router = new Router("http://localhost/projeto_festas.com");
 $event = isset($_GET['event']) ? json_decode(urldecode($_GET['event']), true) : null;
 $error_message = isset($_SESSION['error_message']) ? $_SESSION['error_message'] : '';
 unset($_SESSION['error_message']);
 
 // Verifica se o usuário está logado ou não
 $user_logged_in = isset($_SESSION['logado']);
-$router->get("/login", "Redirect:telaLogin");
-$router->dispatch();
+
+$routes = [
+  '/projeto_festas.com/login' => 'login.view',
+  '/projeto_festas.com/eventos' => 'user_events.view',
+  '/projeto_festas.com/perfil' => 'profile.view',
+  '/projeto_festas.com/cadastrar' => 'register.view'
+];
+
+$path = $_SERVER['REQUEST_URI'];
+
+if ($path != '/projeto_festas.com/'){
+  if (array_key_exists($path, $routes)) {
+    $route = $routes[$path];
+    require_once 'views/' . $route . '.php';
+    exit;
+  }
+}
+
 ?>
 <?php include 'views/header.view.php'; ?>
 <main>
