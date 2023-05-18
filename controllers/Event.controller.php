f875f684490a466b0f9949c9dd53ea9b40fcf538
Erickricheter <erickricheter@hotmail.com>
2023-05-18 13:16:42 -0300

    Adicionado a exibição do banco de dados dentro do front-end.

--- controllers/Event.controller.php
+++ controllers/Event.controller.php
@@ -1,98 +1,72 @@
 <?php
+
 require_once '../vendor/autoload.php';
 require_once __DIR__ . '/../phinx.php';
 
-// Verifique se o formulário foi enviado
+class EventController
+{
+    private $pdo;
+    public function __construct()
+    {
+        $dsn = 'mysql:host=localhost;dbname=development_db';
+        $username = 'root';
+        $password = '';
+        $this->pdo = new PDO($dsn, $username, $password);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function saveEvent($evento, $descricao, $local, $data, $horario)
+    {
+        try {
+            // inserir os dados SQL
+            $sql = 'INSERT INTO evento (evento, descricao, local, data, horario) VALUES (:evento, :descricao, :local, :data, :horario)';
+            $stmt = $this->pdo->prepare($sql);
+
+            // consulta com os valores do formulário
+            $stmt->execute([
+                'evento' => $evento,
+                'descricao' => $descricao,
+                'local' => $local,
+                'data' => $data,
+                'horario' => $horario
+            ]);
+            return true;
+        } catch (PDOException $e) {
+            // Tratar erro consulta SQL
+            echo 'Erro ao salvar os dados: ' . $e->getMessage();
+            return false;
+        }
+    }
+    public function getEventsFromDatabase()
+    {
+        try {
+            $stmt = $this->pdo->query('SELECT * FROM evento');
+            $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $events;
+        } catch (PDOException $e) {
+            // Tratar erro consulta SQL
+            echo 'Erro ao recuperar os eventos: ' . $e->getMessage();
+            return [];
+        }
+    }
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Recupere os valores do formulário
     $evento = $_POST['evento'];
     $descricao = $_POST['descricao'];
     $local = $_POST['local'];
     $data = $_POST['data'];
     $horario = $_POST['horario'];
 
-    // Conexão com o banco de dados usando PDO
-    $dsn = 'mysql:host=localhost;dbname=development_db';
-    $username = 'root';
-    $password = '';
+    // Instanciar o controlador de eventos
+    $eventController = new EventController();
 
-    try {
-        $pdo = new PDO($dsn, $username, $password);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        // Preparar a consulta SQL para inserir os dados
-        $sql = 'INSERT INTO evento (evento, descricao, local, data, horario) VALUES (:evento, :descricao, :local, :data, :horario)';
-        $stmt = $pdo->prepare($sql);
-
-        // Executar a consulta com os valores do formulário
-        $stmt->execute([
-            'evento' => $evento,
-            'descricao' => $descricao,
-            'local' => $local,
-            'data' => $data,
-            'horario' => $horario
-        ]);
-
+    // Salvar o evento no banco de dados
+    if ($eventController->saveEvent($evento, $descricao, $local, $data, $horario)) {
         // Redirecionar ou exibir uma mensagem de sucesso, conforme necessário
         header('Location: ../index.php');
         exit();
-    } catch (PDOException $e) {
-        // Tratar erros de conexão ou consulta SQL
-        echo 'Erro ao salvar os dados: ' . $e->getMessage();
+    } else {
+        // O erro já foi tratado dentro do método saveEvent REMOVER
     }
 }
-
-// class Evento
-// {
-//     private $id;
-//     private $evento;
-//     private $descricao;
-//     private $local;
-//     private $data;
-//     private $horario;
-
-//     public function __construct($evento, $descricao, $local, $data, $horario)
-//     {
-//         $this->id = uniqid();
-//         $this->evento = $evento;
-//         $this->descricao = $descricao;
-//         $this->local = $local;
-//         $this->data = $data;
-//         $this->horario = $horario;
-//     }
-
-//     // Getter e Setter para cada propriedade, se necessário
-
-//     public function toArray()
-//     {
-//         return array(
-//             "id" => $this->id,
-//             "evento" => $this->evento,
-//             "descricao" => $this->descricao,
-//             "local" => $this->local,
-//             "data" => $this->data,
-//             "horario" => $this->horario
-//         );
-//     }
-// }
-
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//     // Recuperando informações do formulário
-//     $evento = $_POST["evento"];
-//     $descricao = $_POST["descricao"];
-//     $local = $_POST["local"];
-//     $data = $_POST["data"];
-//     $horario = $_POST["horario"];
-
-//     // Criando um objeto Evento com as informações
-//     $newEvent = new Evento($evento, $descricao, $local, $data, $horario);
-
-//     // Transformando o objeto em um array
-//     $eventArray = $newEvent->toArray();
-
-//     // Transformando o array em JSON
-//     $jsonEvent = json_encode($eventArray);
-
-//     // Redirecionando para o index.php com os dados do evento
-//     header('Location: ../index.php?event=' . urlencode($jsonEvent));
-// }
--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 session_start();
 require 'controllers/Event.controller.php';
-$event = isset($_GET['event']) ? json_decode(urldecode($_GET['event']), true) : null;
 $error_message = isset($_SESSION['error_message']) ? $_SESSION['error_message'] : '';
 unset($_SESSION['error_message']);
 
 // Verifica se o usuário está logado ou não
 $user_logged_in = isset($_SESSION['logado']);
+
+$eventController = new EventController();
+$events = $eventController->getEventsFromDatabase();
 ?>
 <?php include 'views/header.view.php'; ?>
 <main>
@@ -17,54 +19,28 @@ $user_logged_in = isset($_SESSION['logado']);
         <img src="https://images.pexels.com/photos/1190297/pexels-photo-1190297.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Imagem de nosso evento!" srcset="">
         <div class="event-infos">
           <h4 class="event-title">
-            Teste nome evento
+            Evento em Ponta Grossa!
           </h4>
           <p class="event-date">Data e horário do evento: 12/04/2023 12:05</p>
           <br>
           <p class="event-description">Teste descrição do evento descrição do evento descrição do evento descrição do evento </p>
-
-        </div>
-      </div>
-      <div class="event">
-        <img src="https://images.pexels.com/photos/1190297/pexels-photo-1190297.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Imagem de nosso evento!" srcset="">
-        <div class="event-infos">
-          <h4 class="event-title">
-            Teste nome evento 2
-          </h4>
-          <p class="event-date">Data do evento: 12/04/2023</p>
-          <p class="event-hour">Horário: 12:05</p>
-          <br>
-          <p class="event-description">Teste descrição do evento descrição do evento descrição do evento descrição do evento</p>
-        </div>
-      </div>
-      <div class="event">
-        <img src="https://images.pexels.com/photos/1190297/pexels-photo-1190297.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Imagem de nosso evento!" srcset="">
-        <div class="event-infos">
-          <h4 class="event-title">
-            Teste nome evento 2
-          </h4>
-          <p class="event-date">Data do evento: 12/04/2023</p>
-          <p class="event-hour">Horário: 12:05</p>
-          <br>
-          <p class="event-description">Teste descrição do evento descrição do evento descrição do evento descrição do evento</p>
-
         </div>
       </div>
       <?php
-      if ($event) {
-        if (is_array($event)) {
+      if (!empty($events)) {
+        foreach ($events as $event) {
           echo '<div class="event">';
           echo '<img src="https://images.pexels.com/photos/1190297/pexels-photo-1190297.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Imagem de nosso evento!" srcset="">';
           echo '<div class="event-infos">';
           echo '<h4 class="event-title">' . $event['evento'] . '</h4>';
-          echo '<p class="event-date">Data e horário do evento: ' . $event['data'] . $event['horario'] . '</p>';
+          echo '<p class="event-date">Data e horário do evento: ' . $event['data'] . ' ' . $event['horario'] . '</p>';
           echo '<br>';
           echo '<p class="event-description">' . $event['descricao'] . '</p>';
           echo '</div>';
           echo '</div>';
-        } else {
-          echo "Erro: Não foi possível exibir os eventos.";
         }
+      } else {
+        echo "Erro: Não foi possível exibir os eventos.";
       }
       ?>
     </div>
